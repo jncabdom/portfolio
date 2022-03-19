b1c460c65de9d53e7fa66ae32fb8af7ff0792748
Nicolás Cabrera <jncabdom@gmail.com>
2022-03-19 21:23:11 +0000

    Further configured linting, getting rid of build/lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,37 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  "env": {
+    "browser": true,
+    "es2021": true,
   },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
+  "extends": [
+    "plugin:react/recommended",
+    "google",
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true,
     },
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
+    "ecmaVersion": "latest",
+    "sourceType": "module",
   },
-  'plugins': [
-    'react',
-    '@typescript-eslint',
+  "plugins": [
+    "react",
+    "@typescript-eslint",
   ],
-  'rules': {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  },
+  "rules": {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "quotes": ["error", "double"],
+    "semi": ["error", "always"],
     "comma-dangle": ["error", "only-multiline"],
-    "space-before-function-paren": ["error", "always"]
+    "space-before-function-paren": ["error", "never"],
+    "object-curly-spacing": ["error", "always"],
+    "require-jsdoc": 0
   },
 };
